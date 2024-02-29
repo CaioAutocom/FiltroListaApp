04f2895e0449302e9c628b5a35e29a21ab8a5417
Caio Mendes <caio.mendes@autocom-mg.com.br>
2024-02-29 13:57:42 -0300

    iniciado adição do filtro

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
 
 @NgModule({
     declarations: [],
     imports:[
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatListModule,
+        MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule
     ],
     exports:[
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatListModule,
+        MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule
     ]
 })
 export class AngularMaterialModule{}
